3981d6a0cd8df7a40c94f03a254c1179a7554e43
Sahil Sharma <ssharma2@svsu.edu>
2024-10-22 13:19:55 -0400

    test recapcha for prod

--- config/keys.js
+++ config/keys.js
@@ -13,4 +13,13 @@ module.exports = {
     sendFromEmail: process.env.MSEXCHANGE_SEND_FROM_EMAIL,
     sendToEmail: process.env.MSEXCHANGE_SEND_TO_EMAIL,
   },
+  recaptcha: {
+    // I could not find the secret for a while follow this thread:
+    // https://stackoverflow.com/questions/66755108/recaptcha-secret-key-where-is-it-in-my-google-cloud-recaptcha-account
+    // The first confusion was why my Google Cloud console called it CAPTCHA "Enterprise".
+    //  But I will leave that exploration to you, as it doesn't matter in our case.
+    // But I was finally able to get it to work. I found the secret under,
+    // Legacy reCAPTCHA secret key
+    secret: process.env.RECAPTCHA_SECRET,
+  },
 };
--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -1,10 +1,16 @@
+const axios = require('axios');
 const emailServices = require('../services/email.services');
+const keys = require('../config/keys');
 
 exports.sendEmail = async (req, res) => {
   const {
-    name, email, subject, message,
+    name, email, subject, message, 'g-recaptcha-response': recaptchaToken,
   } = req.body;
   try {
+    const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${keys.recaptcha.secret}&response=${recaptchaToken}`);
+    if (!recaptchaResponse.data.success) {
+      return res.status(400).json({ message: 'reCAPTCHA verification failed' });
+    }
     const respose = await emailServices.sendEmail(name, email, subject, message);
 
     if (!respose) return res.status(500).json({ message: 'Error sending email' });
