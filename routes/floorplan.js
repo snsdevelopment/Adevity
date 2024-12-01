cf621c8a485fd9251fc43ebfb4c7d4eec647a48b
Sahil Sharma <ssharma2@svsu.edu>
2024-12-01 12:31:20 -0500

    floor plan prices inclusion

--- routes/floorplan.js
+++ routes/floorplan.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.render('floorplan/index', { title: 'Floor Plan - Coming Soon' });
+  res.render('floorplan/floorplan', { title: 'Floor Plan - Coming Soon' });
 });
 
 module.exports = router;
